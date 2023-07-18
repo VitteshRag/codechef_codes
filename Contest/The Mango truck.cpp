3c78ee1185409ee89536b0dd8eaa62c0b1f64e47
Vittesh Raghavan <vitteshraghavan@gmail.com>
2023-07-18 19:53:50 +0530

    Added Changes

---
+++ Contest/The Mango truck.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<(int)(c-b)/a<<endl;
+	}
+	return 0;
+}
