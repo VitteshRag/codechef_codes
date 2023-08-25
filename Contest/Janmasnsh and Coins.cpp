1e4b2606c0e60219d13c084eeea436cfff87152e
Vittesh Raghavan <vitteshraghavan@gmail.com>
2023-08-25 22:37:40 +0530

    Added Chef in his Office solution

---
+++ Contest/Janmasnsh and Coins.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<(a*10)+(b*5)<<endl;
+	}
+	return 0;
+}
